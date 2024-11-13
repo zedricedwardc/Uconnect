bd2bcc4363fdbbb60a86c8e14d74bed1adc8af4b
zed <hernandezzedricedward@gmail.com>
2024-11-14 02:23:24 +0800

    2- authentication

---
+++ backend/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose, { mongo } from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        //member since nov 2024 createdAt
+        username:{
+            type: String,
+            require: true,
+            unique: true,
+        },
+        fullName:{
+            type: String,
+            require: true,
+        },
+        password:{
+            type: String,
+            require: true,
+            minLength: 6,
+        },
+        email:{
+            type: String,
+            require: true,
+            unique: true,
+        },
+        followers:[
+            {
+                type: mongoose.Schema.Types.ObjectId, //reference to user model which had 16 characters
+                ref: "User",
+                default: []
+            }
+        ],
+        following:[
+            {
+                type: mongoose.Schema.Types.ObjectId, //reference to user model which had 16 characters
+                ref: "User",
+                default: []
+            }
+        ],
+
+        profileImg:{
+            type: String,
+            default: "",
+        },
+        coverImage:{
+            type: String,
+            default: "",
+        },
+        Bio:{
+            type: String,
+            default: "",
+        },
+        link:{
+            type: String,
+            default: "",
+        },
+    },
+{timestamps: true}
+);
+
+const User = mongoose.model("User",userSchema); //users in database
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(8000,() => {
+app.listen(PORT,() => {
     console.log('server running on port' , PORT);
     connectMongoDB();
 });
