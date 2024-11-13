5bf1fe98753da34767c430c24f0bc06631b4c3e8
zed <hernandezzedricedward@gmail.com>
2024-11-14 00:26:36 +0800

    1- setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use("/api/auth",authRoutes);
+
+app.listen(8000,() => {
+    console.log('server running on port' , PORT);
+    connectMongoDB();
+});
