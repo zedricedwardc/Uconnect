bfbbcfb2c984bc7b97b64693de356e0f1f53c9ef
Jian Klievert <jianklievertm@gmail.com>
2024-11-17 14:07:05 +0800

    Added posts and comments palang. after na like

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
 
-import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 
@@ -27,6 +28,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
 
 
 app.listen(PORT,() => {
