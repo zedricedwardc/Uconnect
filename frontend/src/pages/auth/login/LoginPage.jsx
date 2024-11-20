f7cca200e81dfa9f17f03ce5305c562d99c492d7
zed <hernandezzedricedward@gmail.com>
2024-11-21 00:55:52 +0800

    2 -UI setup

---
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LoginPage = () => {
+    console.log('Login rendered');
+    return (
+        <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SignUpPage = () => {
+    console.log('Sign up rendered');
+    return (
+        <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+        </div>
+    );
+};
+
+export default SignUpPage;
