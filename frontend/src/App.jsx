cdcd85d8e7cfdc1a0dd37d9f4da36ce08385b9e1
Jian Klievert <jianklievertm@gmail.com>
2024-12-03 23:10:56 +0800

    login, logout, signup. Auth in React && Protected Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-
 import HomePage from "./pages/auth/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
@@ -9,18 +8,54 @@ import RightPanel from "./pages/auth/components/common/RighPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./pages/auth/components/common/LoadingSpinner";
+import { Navigate } from "react-router-dom";
+
+
 function App() {
+  const { data: authUser, isLoading } = useQuery({
+    // we use queryKey to give a unique name to our query and refer to it later
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch('/api/auth/me');
+        const data = await res.json();
+        if (data.error) return null;
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("authUser is here:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className='h-screen flex justify-center items-center'>
+        <LoadingSpinner size='lg' />
+      </div>
+    )
+  }
+
   return (
     <div className='flex max-w-6xl mx-auto'>
-    <Sidebar/>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path='/notifications' element={<NotificationPage />} />
-      <Route path='/profile/:username' element={<ProfilePage />} />
-    </Routes>
-    <RightPanel/>
+      {/* Common component, bc it's not wrapped with routes */}
+      {authUser && <Sidebar />}
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
+      {authUser && <RightPanel />}
+      <Toaster />
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-			<App />
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <QueryClientProvider client = {queryClient}>
+			  <App />
+      </QueryClientProvider>
 		</BrowserRouter>
   </StrictMode>,
 )
