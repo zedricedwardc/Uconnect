3f79a8c9cc90ba6bc897b423d87e40da0d4de66d
zed <hernandezzedricedward@gmail.com>
2024-12-05 11:51:23 +0800

    sugested users

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
